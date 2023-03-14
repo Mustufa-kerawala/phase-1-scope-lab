2f7ef305ca0ed5858b723ed207b2c7571b591d93
Mustufa-kerawala <mustufakerawala@outlook.com>
2023-03-14 16:12:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'bob';
+// console.log(customerName);
+
+function upperCaseCustomerName() {
+    customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'maybe bob';
+    // leastFavoriteCustomer = 'not bob';
+}
+
+
+console.log(leastFavoriteCustomer);
